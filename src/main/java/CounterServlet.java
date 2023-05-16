18a6dc49a57d601724cc84eecf528fc6c76ddd8d
charlesrolo22 <charles.a.roland@gmail.com>
2023-05-16 14:57:08 -0500

    new update

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+
+    private int count = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        count++;
+
+        String reset = req.getParameter("reset");
+
+        if (reset != null) {
+            count = 0;
+    }
+
+        out.println("<h1>" + count + "</h1>");
+}
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -10,7 +10,7 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
             res.setContentType("text/html");
             PrintWriter out = res.getWriter();
-            out.println("<h1>hello world</h1>");
+            out.println("<h1>Hello, World</h1>");
 
     }
 }
