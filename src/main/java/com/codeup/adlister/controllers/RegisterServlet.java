0eecc810c1c335c77b823da2ef03c497c0eacf9b
charlesrolo22 <charles.a.roland@gmail.com>
2023-05-30 09:07:16 -0500

    passwords

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,41 +1,27 @@
-package com.codeup.adlister.controllers;
-
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
-public class RegisterServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
-    }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String username = request.getParameter("username");
-        String email = request.getParameter("email");
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
 
+        String hashWord = BCrypt.hashpw(password, BCrypt.gensalt());
+
+
         // validate input
         boolean inputHasErrors = username.isEmpty()
-            || email.isEmpty()
-            || password.isEmpty()
-            || (! password.equals(passwordConfirmation));
-
-        if (inputHasErrors) {
-            response.sendRedirect("/register");
-            return;
+        || email.isEmpty()
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
         }
 
         // create and save a new user
         User user = new User(username, email, password);
+        User user = new User(username, email, hashWord);
         DaoFactory.getUsersDao().insert(user);
         response.sendRedirect("/login");
-    }
-}
+        }
