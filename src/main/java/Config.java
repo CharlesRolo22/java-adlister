38c76bd041b85ff0fe76887f0d41986db31f8325
charlesrolo22 <charles.a.roland@gmail.com>
2023-06-05 10:58:32 -0500

    add gitignore

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,17 +1,11 @@
-public class Config {
+class Config {
     public String getUrl() {
-        // Return the database URL
-        "jdbc:mysql://localhost:3306/adlister_db_db?allowPublicKeyRetrieval=true&useSSL=false",
-                "root",
+        return "jdbc:mysql://localhost:3306/codeup_test_db?allowPublicKeyRetrieval=true&useSSL=false";
     }
-
-    public String getUsername() {
-        // Return the database username
-        return "adlister_user";
+    public String getUser() {
+        return "root";
     }
-
     public String getPassword() {
-        // Return the database password
-        return "password";
+        return "codeup";
     }
 }
